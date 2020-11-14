2493b81bc6d88e9cdac48d3e2eb0b06fe132ebae
Reyhan <reyhannaufal21@gmail.com>
2020-11-14 23:22:30 +0700

    feat: setting vue dev enviroment

---
+++ resources/js/pages/WelcomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="counter">
+      Counter: {{counter}}
+  </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return{
+            counter: 0
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
